73bd8424e183bc16ad7dd6868c04f66950f4d7c8
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2020-07-15 18:08:23 +0200

    Add publish to MAM using MAM.js

--- mam.js/publish-mam.js
+++ mam.js/publish-mam.js
@@ -1,66 +1,71 @@
-const Mam = require("@iota/mam");
+const Iota = require("@iota/core");
+const {
+  channelRoot,
+  createChannel,
+  createMessage,
+  mamAttach,
+} = require("@iota/mam.js");
 const { asciiToTrytes } = require("@iota/converter");
 
 const providerName = "devnet";
 
 const mamExplorerLink = "https://utils.iota.org/mam";
 
-const seed =
-  "SNBGLXRCLW9MYEKGEFSAUDECOYHTTWOKPVSZAWJYIJAYMCNGYIRPDLJUKZXVVWRN9ZFUPHXEGYRGOXMXH";
+const modes = ["public", "private", "restricted"];
 
-async function publish(mode, network, packet, startIndex, sideKey) {
+async function publish({ seed, mode, network, packet, startIndex, sideKey }) {
+  // console.log("Publish: ", arguments[0]);
   // Initialise MAM State
-  let mamState = Mam.init(network, seed);
-  mamState = Mam.changeMode(mamState, mode, sideKey);
+  const channelState = createChannel(seed, 2, mode, sideKey);
 
-  const treeRoot = Mam.getRoot(mamState);
+  const treeRoot = channelRoot(channelState);
 
-  mamState.channel.start = startIndex;
+  channelState.start = startIndex;
+  const mamMessage = createMessage(channelState, asciiToTrytes(packet));
 
-  // Create MAM Payload - STRING OF TRYTES
-  const trytes = asciiToTrytes(packet);
-  const message = Mam.create(mamState, trytes);
-
-  // Attach the payload
-  await Mam.attach(message.payload, message.root, 3, 9);
+  // And then attach the message, tagging it if required.
+  // Attaching will return the actual transactions attached to the tangle if you need them.
+  const api = Iota.composeAPI({ provider: network });
+  await mamAttach(api, mamMessage, 3, 9);
 
   return {
     treeRoot,
-    thisRoot: message.root,
-    nextIndex: mamState.channel.start,
+    thisRoot: mamMessage.address,
+    nextIndex: channelState.start,
   };
 }
 
-const modes = ["public", "private", "restricted"];
+if (process.argv.length >= 6) {
+  const seed = process.argv[2];
 
-if (process.argv.length >= 5) {
-  const mode = process.argv[2];
+  const mode = process.argv[3];
   if (modes.indexOf(mode) === -1) {
     console.error(`Error: Mode must be one of: ${modes}`);
     process.exit(1);
   }
 
-  const network = process.argv[3];
+  const network = process.argv[4];
 
-  const messageObj = JSON.parse(process.argv[4]);
+  const messageObj = JSON.parse(process.argv[5]);
   messageObj.timestamp = new Date().toISOString();
   const message = JSON.stringify(messageObj);
 
   // It might be undefined
-  let startIndex = process.argv[5];
+  let startIndex = process.argv[6];
   startIndex = startIndex || "0";
   startIndex = parseInt(startIndex);
 
   // It might be undefined
-  const sideKey = process.argv[6];
+  const sideKey = process.argv[7];
   if (mode === "restricted" && !sideKey) {
     console.error("Error: In restricted mode you need to provide a side key");
     process.exit(1);
   }
 
-  publish(mode, network, message, startIndex, sideKey).then(
+  publish({ mode, seed, network, packet: message, startIndex, sideKey }).then(
     ({ treeRoot, thisRoot, nextIndex }) => {
       const result = {
+        seed,
         treeRoot: formatExplorerURI(mode, treeRoot, sideKey),
         thisRoot: formatExplorerURI(mode, thisRoot, sideKey),
         nextIndex,
@@ -70,7 +75,7 @@ if (process.argv.length >= 5) {
   );
 } else {
   console.log(
-    "Usage: publish-mam <mode> <network> <message> <startIndex> <sideKey>"
+    "Usage: publish-mam <seed> <mode> <network> <message> <startIndex> <sideKey>"
   );
 }
 
