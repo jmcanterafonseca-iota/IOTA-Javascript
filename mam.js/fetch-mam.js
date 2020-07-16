7790fa5bf6acb0890217f39a8a183af7208be308
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2020-07-16 11:49:56 +0200

    Allow to watch or not watch while fetching

--- mam.js/fetch-mam.js
+++ mam.js/fetch-mam.js
@@ -6,28 +6,34 @@ const modes = ["public", "private", "restricted"];
 
 const INTERVAL = 5000;
 
-function fetchMamChannel(network, mode, root, sideKey) {
+function fetchMamChannel(network, mode, root, sideKey, watch) {
+  let doWatch = watch;
+
+  if (typeof watch === "undefined") {
+    doWatch = false;
+  }
+
   try {
     // Initialise IOTA API
     const api = Iota.composeAPI({ provider: network });
-    retrieve(api, root, mode, sideKey);
+    retrieve(api, root, mode, sideKey, doWatch);
   } catch (error) {
     console.error("Error while fetching MAM Channel: ", error);
     process.exit(-1);
   }
 }
 
-function retrieve(api, root, mode, sideKey) {
+function retrieve(api, root, mode, sideKey, watch) {
   let currentRoot = root;
 
-  setInterval(async () => {
+  const retrievalFunction = async () => {
     try {
       // console.log('Current Root', currentRoot);
 
       const fetched = await mamFetchAll(api, currentRoot, mode, sideKey);
 
       fetched.forEach((result) => {
-        console.log(JSON.parse(trytesToAscii(result.message)), "\n");
+        console.log(JSON.parse(trytesToAscii(result.message)));
       });
 
       if (fetched.length > 0) {
@@ -38,11 +44,19 @@ function retrieve(api, root, mode, sideKey) {
     } catch (error) {
       console.error("Error while fetching MAM Channel: ", error);
     }
-  }, INTERVAL);
+  };
+
+  if (watch === true) {
+    setInterval(retrievalFunction, INTERVAL);
+  } else {
+    retrievalFunction();
+  }
 }
 
 if (process.argv.length >= 5) {
   const network = process.argv[3];
+
+  let watch = false;
 
   const mode = process.argv[2];
   if (modes.indexOf(mode) === -1) {
@@ -52,14 +66,30 @@ if (process.argv.length >= 5) {
 
   const root = process.argv[4];
 
-  // It might be undefined
-  const sideKey = process.argv[5];
-  if (!sideKey && mode === "restricted") {
+  // It might be undefined or the watch flag
+  let sideKey = process.argv[5];
+  if (!sideKey) {
+    if (mode === "restricted") {
+      console.error("Error: Restricted mode requires a side key");
+      process.exit(1);
+    }
+  } else if (sideKey === "-w" && mode === "restricted") {
     console.error("Error: Restricted mode requires a side key");
     process.exit(1);
+  } else if (sideKey === "-w") {
+    sideKey = null;
+    watch = true;
   }
 
-  fetchMamChannel(network, mode, root, sideKey);
+  if (!watch) {
+    // It might be undefined
+    const watchParam = process.argv[6];
+    if (watchParam === "-w") {
+      watch = true;
+    }
+  }
+
+  fetchMamChannel(network, mode, root, sideKey, watch);
 } else {
-  console.log("Usage: fetch-mam <mode> <network> <root> <sideKey>");
+  console.log("Usage: fetch-mam <mode> <network> <root> <sideKey> -w");
 }
---
+++ publish.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+numMessages=$1
+seed=$(./test-data/seed.sh)
+sideKey=$(./test-data/seed.sh)
+network=$(cat ./test-data/devnet)
+message=$(cat ./test-data/message.json)
+
+echo $numMessages
+echo $seed
+echo $sideKey
+echo $network
+echo $message
+
+for i in {0..1000}
+  do echo "$i"; node mam.js/publish-mam.js $seed restricted $network "$message" $i $sideKey;
+done
+
